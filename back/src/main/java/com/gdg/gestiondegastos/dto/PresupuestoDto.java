3aa348f77a27d2e88c80af8aa196c44cc6f0e459
Raúl Fernández <fmedinaraul@gmail.com>
2021-09-28 14:18:28 +0200

    Todos los Dto realizados

---
+++ back/src/main/java/com/gdg/gestiondegastos/dto/PresupuestoDto.java
@@ -0,0 +1,28 @@
+package com.gdg.gestiondegastos.dto;
+
+import com.gdg.gestiondegastos.entities.Presupuesto;
+import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class PresupuestoDto {
+    private Integer id;
+    private Double cantidadInicio;
+    private Double cantidadFinal;
+    private Date fechaInicio;
+    private Date fechaFinal;
+
+    public PresupuestoDto(Presupuesto pre) {
+        id = pre.getId();
+        cantidadInicio = pre.getCantidadInicio();
+        cantidadFinal = pre.getCantidadFinal();
+        fechaInicio = pre.getFechaInicio();
+        fechaFinal = pre.getFechaFinal();
+    }
+}
