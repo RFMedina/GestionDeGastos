e671c051684bf7120a0c1dfa17415ba05d5c516f
Daiggo <diegopg99@gmail.com>
2021-10-04 12:20:13 +0200

    BBDD cambio CURDATE() ->  NOW()

--- src/main/java/com/gdg/gestiondegastos/services/GeneracionDatos.java
+++ src/main/java/com/gdg/gestiondegastos/services/GeneracionDatos.java
@@ -5,12 +5,12 @@
  */
 package com.gdg.gestiondegastos.services;
 
+import com.gdg.gestiondegastos.entities.Grupo;
 import com.gdg.gestiondegastos.entities.Usuario;
-import com.gdg.gestiondegastos.entities.UsuarioGrupo;
 import com.gdg.gestiondegastos.interfaces.IGeneracionDatos;
+import com.gdg.gestiondegastos.repositories.GrupoRepository;
 import com.gdg.gestiondegastos.repositories.UsuarioRepository;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -25,19 +25,30 @@ public class GeneracionDatos implements IGeneracionDatos{
     private UsuarioRepository usuarios;
 
     @Autowired
+    private GrupoRepository grupos;
+    
+    @Autowired
     private PasswordEncoder encoder;
     
+    private Date fecha = new Date();
     @Override
     public void generarDatos() {
         if(usuarios.findAll().iterator().hasNext() == false){
+            //USERS
             usuarios.save(
-                    new Usuario(1, "Pepe", encoder.encode("1111"), "1234", "mail@mail.com", true, null));
-        
+                    new Usuario(1, "Raul", encoder.encode("Raul"), "1234", "Raul@gmail.com", true, null));
             usuarios.save(
-                    new Usuario(2, "Marta", encoder.encode("1111"), "565656", "mail2@mail.com", true, null));
-        
+                    new Usuario(2, "Admin", encoder.encode("Admin"), "565656", "Admin@gmail.com", true, null));
             usuarios.save(
-                    new Usuario(3, "Sara", encoder.encode("1111"), "444444", "mail3@mail.com", true, null));
+                    new Usuario(3, "Sofia", encoder.encode("Sofia"), "444444", "Sofia@gmail.com", true, null));
+            usuarios.save(
+                    new Usuario(3, "Martin", encoder.encode("Martin"), "444444", "Martin@gmail.com", true, null));
+            usuarios.save(
+                    new Usuario(3, "Jose María", encoder.encode("Josema"), "444444", "JoseMa@gmail.com", true, null));
+            
+            //GRUPOS
+            grupos.save(
+                    new Grupo(1,"Mi Grupo",null,null));
         }
         
     }
